001d57698c59808a2ecff89f5362c3de48231296
Shashank <shashankjain.ips@gmail.com>
2016-09-24 10:17:55 +0530

    Initial checkin

---
+++ TestNg/src/testNgTest.java
@@ -0,0 +1,39 @@
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class testNgTest {
+	
+
+@Test
+public void Test1()
+{
+	System.out.println("Test1 invoked");
+}
+@Test
+public void Test2()
+{
+	System.out.println("Test2 invloked");
+}
+@org.testng.annotations.BeforeMethod
+public void BeforeMethod()
+{
+	System.out.println("Before method invoked");
+}
+	
+@org.testng.annotations.AfterMethod
+public void AfterMethod()
+{
+	System.out.println("After method invoked");
+	
+}
+@org.testng.annotations.BeforeClass
+public void BeforeClass(){
+	System.out.println("Before class invoked");
+}
+//@org.testng.annotations.AfterClass
+//public void AfterClass(){
+//	System.out.println("After class in invoked");
+//}
+}
+
